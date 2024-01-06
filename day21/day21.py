5632409addcfb375a2dd76cd8f7f4f8281b7d230
Jay Zhou <j.zhou@uwaterloo.ca>
2024-01-06 18:37:16 -0500

    Day21p1

---
+++ day21/day21.py
@@ -0,0 +1,106 @@
+"""
+He needs to get his steps in for the day, and so he'd like to know which garden plots he can reach with exactly his remaining 64 steps.
+
+He gives you an up-to-date map (your puzzle input) of his starting position (S), garden plots (.), and rocks (#). For example:
+
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+The Elf starts at the starting position (S) which also counts as a garden plot. Then, he can take one step north, south, east, or west, but only onto tiles that are garden plots. This would allow him to reach any of the tiles marked O:
+
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#O#....
+.##.OS####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+Then, he takes a second step. Since at this point he could be at either tile marked O, his second step would allow him to reach any garden plot that is one step north, south, east, or west of any tile that he could have reached after the first step:
+
+...........
+.....###.#.
+.###.##..#.
+..#.#O..#..
+....#.#....
+.##O.O####.
+.##.O#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+After two steps, he could be at any of the tiles marked O above, including the starting position (either by going north-then-south or by going west-then-east).
+
+A single third step leads to even more possibilities:
+
+...........
+.....###.#.
+.###.##..#.
+..#.#.O.#..
+...O#O#....
+.##.OS####.
+.##O.#...#.
+....O..##..
+.##.#.####.
+.##..##.##.
+...........
+He will continue like this until his steps for the day have been exhausted. After a total of 6 steps, he could reach any of the garden plots marked O:
+
+...........
+.....###.#.
+.###.##.O#.
+.O#O#O.O#..
+O.O.#.#.O..
+.##O.O####.
+.##.O#O..#.
+.O.O.O.##..
+.##.#.####.
+.##O.##.##.
+...........
+In this example, if the Elf's goal was to get exactly 6 more steps today, he could use them to reach any of 16 garden plots.
+
+However, the Elf actually needs to get 64 steps today, and the map he's handed you is much larger than the example map.
+
+Starting from the garden plot marked S on your map, how many garden plots could the Elf reach in exactly 64 steps?
+"""
+import sys
+# from collections import deque
+
+# Idea: https://www.reddit.com/r/adventofcode/comments/18ltr8m/comment/ke010be/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+def solve():
+    matrix = [list(line.strip()) for line in input]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == "S":
+                q = set([(i, j)])
+                matrix[i][j] = "."
+                break
+
+    for _ in range(64):
+        next_q = set()
+        for i, j in q:
+            for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                if x in range(len(matrix)) and y in range(len(matrix[0])) and matrix[x][y] == ".":
+                    next_q.add((x, y))
+        q = next_q
+    print(len(q))
+    # print(sorted(list(q)))
+
+if __name__ == "__main__":
+    # take input file name from cmd, default to input.txt
+    if len(sys.argv) > 1:
+        input = open(sys.argv[1], "r").readlines()
+    else:
+        input = open("input.txt", "r").readlines()
+    solve()
