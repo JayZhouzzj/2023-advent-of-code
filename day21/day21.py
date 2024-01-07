598d0627dd1eb0273c1762b0de043c8a3d70ad13
Jay Zhou <j.zhou@uwaterloo.ca>
2024-01-06 19:20:22 -0500

    Day21p2

--- day21/day21.py
+++ day21/day21.py
@@ -73,11 +73,66 @@ In this example, if the Elf's goal was to get exactly 6 more steps today, he cou
 However, the Elf actually needs to get 64 steps today, and the map he's handed you is much larger than the example map.
 
 Starting from the garden plot marked S on your map, how many garden plots could the Elf reach in exactly 64 steps?
+--- Part Two ---
+The Elf seems confused by your answer until he realizes his mistake: he was reading from a list of his favorite numbers that are both perfect squares and perfect cubes, not his step counter.
+
+The actual number of steps he needs to get today is exactly 26501365.
+
+He also points out that the garden plots and rocks are set up so that the map repeats infinitely in every direction.
+
+So, if you were to look one additional map-width or map-height out from the edge of the example map above, you would find that it keeps repeating:
+
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##..S####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+.................................
+.....###.#......###.#......###.#.
+.###.##..#..###.##..#..###.##..#.
+..#.#...#....#.#...#....#.#...#..
+....#.#........#.#........#.#....
+.##...####..##...####..##...####.
+.##..#...#..##..#...#..##..#...#.
+.......##.........##.........##..
+.##.#.####..##.#.####..##.#.####.
+.##..##.##..##..##.##..##..##.##.
+.................................
+This is just a tiny three-map-by-three-map slice of the inexplicably-infinite farm layout; garden plots and rocks repeat as far as you can see. The Elf still starts on the one middle tile marked S, though - every other repeated S is replaced with a normal garden plot (.).
+
+Here are the number of reachable garden plots in this new infinite version of the example map for different numbers of steps:
+
+In exactly 6 steps, he can still reach 16 garden plots.
+In exactly 10 steps, he can reach any of 50 garden plots.
+In exactly 50 steps, he can reach 1594 garden plots.
+In exactly 100 steps, he can reach 6536 garden plots.
+In exactly 500 steps, he can reach 167004 garden plots.
+In exactly 1000 steps, he can reach 668697 garden plots.
+In exactly 5000 steps, he can reach 16733044 garden plots.
+However, the step count the Elf needs is much larger! Starting from the garden plot marked S on your infinite map, how many garden plots could the Elf reach in exactly 26501365 steps?
 """
 import sys
-# from collections import deque
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-# Idea: https://www.reddit.com/r/adventofcode/comments/18ltr8m/comment/ke010be/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 def solve():
     matrix = [list(line.strip()) for line in input]
     for i in range(len(matrix)):
@@ -86,16 +141,42 @@ def solve():
                 q = set([(i, j)])
                 matrix[i][j] = "."
                 break
+    # x_arr = []
+    # y_arr = []
+    # for iteration in range(1, 1000):
+    #     next_q = set()
+    #     for i, j in q:
+    #         for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+    #             mx = x % len(matrix)
+    #             my = y % len(matrix[0])
+    #             if matrix[mx][my] == ".":
+    #                 next_q.add((x, y))
+    #     q = next_q
+    #     if iteration % 131 == 65:
+    #         x_arr.append(iteration)
+    #         y_arr.append(len(q))
+    #         print(iteration, len(q))
+    # x_data = np.array(x_arr)
+    # y_data = np.array(y_arr)
+    x_data = np.array([65, 196, 327, 458])
+    y_data = np.array([3797, 34009, 94353, 184829])
+    # Polynomial fitting idea: https://www.reddit.com/r/adventofcode/comments/18nevo3/comment/keao4q8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    def polynomial(x, a, b, c):
+        return a * x**2 + b * x + c
 
-    for _ in range(64):
-        next_q = set()
-        for i, j in q:
-            for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                if x in range(len(matrix)) and y in range(len(matrix[0])) and matrix[x][y] == ".":
-                    next_q.add((x, y))
-        q = next_q
-    print(len(q))
-    # print(sorted(list(q)))
+    # Fit the polynomial
+    popt, _ = curve_fit(polynomial, x_data, y_data)
+
+    # Extrapolating
+    x_extrapolate = np.arange(500, 510)  # Range to extrapolate
+    y_extrapolate = polynomial(x_extrapolate, *popt)
+
+    # Plotting
+    plt.scatter(x_data, y_data, label='Original Data')
+    plt.plot(x_extrapolate, y_extrapolate, color='red', label='Extrapolation')
+    plt.legend()
+    plt.show()
+    print(polynomial(26501365, *popt))
 
 if __name__ == "__main__":
     # take input file name from cmd, default to input.txt
